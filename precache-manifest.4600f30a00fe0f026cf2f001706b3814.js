530d8f68b22e1849d0b462542e24a0df74381438
mSapp <matt.sapp1423@gmail.com>
2019-10-30 19:20:30 -0700

    Updates

---
+++ precache-manifest.4600f30a00fe0f026cf2f001706b3814.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6199615a43df296ab6c10e184d885349",
+    "url": "/RoboFrans/index.html"
+  },
+  {
+    "revision": "b77b6743426717f7caa2",
+    "url": "/RoboFrans/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "a1c5847bdc8411fdc0e3",
+    "url": "/RoboFrans/static/css/main.0f0f8c44.chunk.css"
+  },
+  {
+    "revision": "b77b6743426717f7caa2",
+    "url": "/RoboFrans/static/js/2.f9b2e496.chunk.js"
+  },
+  {
+    "revision": "a1c5847bdc8411fdc0e3",
+    "url": "/RoboFrans/static/js/main.55c4ea2a.chunk.js"
+  },
+  {
+    "revision": "a9cbac77b98b6cdce2dc",
+    "url": "/RoboFrans/static/js/runtime-main.7a65936d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFrans/static/media/SEGA.12a3db8b.woff"
+  }
+]);
